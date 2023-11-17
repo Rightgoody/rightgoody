393af6566f7329a918b57f3a036354349e010e47
Rightgoody <123918313+Rightgoody@users.noreply.github.com>
2023-11-17 09:30:37 +0000

    updating repo!

---
+++ codingproblem/coding_problem.cc
@@ -0,0 +1,18 @@
+// this problem is to solve a question during office hours
+
+#include <iostream>
+
+int main() {
+
+int power_num;
+int user_input;
+
+std::cout << "please enter a number \n";
+std::cin >> user_input;
+
+if ((user_input % 3) 0= 0){
+    power_num = (user_input / 3);
+    std::cout <<
+}
+
+}
