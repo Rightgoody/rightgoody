1670912322982b0ae0ae0927076ace20c47da016
Rightgoody <123918313+Rightgoody@users.noreply.github.com>
2023-11-17 09:59:10 +0000

    program looking good so far! i'm sure better ways to write, but definetely a solution :)

--- codingproblem/coding_problem.cc
+++ codingproblem/coding_problem.cc
@@ -10,9 +10,21 @@ int user_input;
 std::cout << "please enter a number \n";
 std::cin >> user_input;
 
-if ((user_input % 3) 0= 0){
+if ((user_input % 3) == 0) {
     power_num = (user_input / 3);
-    std::cout <<
+    std::cout << "The highest product from the "
+                 "sum of the given number is 3^" 
+              << power_num << ".\n";
+} else if (((user_input - 1) % 3) == 0) {
+    power_num = ((user_input - 1) / 3);
+    std::cout << "The highest product from the "
+                 "sum of the given number is 2^2 * 3^"
+              << power_num << ".\n";
+} else if (((user_input - 2) % 3) == 0) {
+    power_num = ((user_input - 2) / 3);
+    std::cout << "The highest product from the "
+                 "sum of the given number is 2 * 3^"
+              << power_num << ".\n";
 }
-
+return 0;
 }
